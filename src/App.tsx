4d4bb154ba661238285130fd3374f9705810883c
Kevin Sebastian <116342133+kevinssheva@users.noreply.github.com>
2024-02-26 16:26:38 +0700

    feat: a bit design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="font-bold text-5xl">HALO</div>;
+  const [input, setInput] = useState("");
+  return (
+    <div className="w-full h-screen bg-purple-400 flex items-center justify-center">
+      <div className="w-full max-w-xl bg-white py-7 px-10 rounded-lg">
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="bg-zinc-200 px-4 py-3"
+          placeholder="Enter your text here..."
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
